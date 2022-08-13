80c56fa49bc31a8caf2895ecc878f2e498ea3b5f
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-08-13 10:17:02 -0300

    inserting objecct into database

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,17 +1,25 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa pessoa = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa pessoa2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa pessoa3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa pessoa = em.find(Pessoa.class, 2);
+		
 		System.out.println(pessoa);
-		System.out.println(pessoa2);
-		System.out.println(pessoa3);
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 	}
 
 }
